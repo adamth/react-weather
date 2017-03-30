c9438f2252b266286f0c49078a8d434754b59abd
Adam Thompson <Adam@Adams-MacBook-Pro.local>
2017-03-30 21:57:07 +1100

    Update About page content and style

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,10 +1,23 @@
 const React = require('react');
+const {Link} = require('react-router');
 
 var About = (props) => {
     return (
         <div>
-            <h3>About</h3>
-            <p>Welcome to the about page!</p>
+            <h1 className="text-center">About</h1>
+            <p>Use this app to find the weather in any city.</p>
+            <p>Here are some of the tools I used:</p>
+            <ul>
+                <li>
+                    <a href="https://facebook.github.io/react/">React</a> - this was the main
+                    JavaScript framework.
+                </li>
+                <li>
+                    <a href="https://openweathermap.org/">OpenWeatherMap</a> - I used
+                    Open Weather map to search for the weather in different cities.
+                </li>
+            </ul>
+            <a href="https://github.com/adamth/react-weather">GitHub</a> repository for the project is available here.
         </div>           
         );
 };
