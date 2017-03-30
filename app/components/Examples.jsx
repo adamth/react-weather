8a1c4e10ba4934e32e3f1b68721566c3bc837a70
adamth <adam@adamth.com>
2017-03-30 16:50:03 +1100

    Style examples page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 const React = require('react');
+const {Link} = require('react-router');
 
 var Examples = (props) => {
     return (
         <div>
-            <h3>Examples</h3>
-            <p>Welcome to the examples page!</p>
+            <h1 className="text-center">Examples</h1>
+            <p>Here are a few example lcoations to try out:</p>
+            <ol>
+                <li>
+                    <Link to='/?location=Melborune'>Melbourne, AU</Link>
+                </li>
+                <li>
+                    <Link to='/?location=London'>London, UK</Link>
+                </li>
+            </ol>
         </div>         
         );
 };
