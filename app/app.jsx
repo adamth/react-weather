89874100f54ccf888fd37f2cf3cb51e6156e7b67
adamth <adam@adamth.com>
2017-03-31 16:37:22 +1100

    Add custom styles

--- app/app.jsx
+++ app/app.jsx
@@ -8,6 +8,8 @@ const Examples = require('Examples');
 
 // Load foundation
 require('style!css!foundation-sites/dist/css/foundation.min.css');
+// app.css
+require('style!css!applicationStyles');
 $(document).foundation();
 
 ReactDOM.render(
